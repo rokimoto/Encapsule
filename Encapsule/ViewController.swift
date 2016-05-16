883a7705935637ee4d6e8129f423ab0d3893ddd5
Rachel Okimoto <rachel.okimoto@gmail.com>
2016-05-15 17:25:24 -0700

    Initial Commit

---
+++ Encapsule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Encapsule
+//
+//  Created by momo on 5/15/16.
+//  Copyright © 2016 Rachel Okimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
