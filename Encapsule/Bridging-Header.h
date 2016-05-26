db67d18d3c7ec0fc6d00f014bc7733d8e1efdbb8
Rachel Okimoto <rachel.okimoto@gmail.com>
2016-05-25 20:14:29 -0700

    added facebook oauth and login

---
+++ Encapsule/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  Encapsule
+//
+//  Created by Rachel Okimoto on 5/25/16.
+//  Copyright © 2016 Rachel Okimoto. All rights reserved.
+//
+
+
+#ifndef FacebookTutorial_Bridging_Header_h
+#define FacebookTutorial_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
--- Encapsule/FacebookFriendsViewController.swift
+++ Encapsule/FacebookFriendsViewController.swift
@@ -6,8 +6,64 @@
 //  Copyright © 2016 Rachel Okimoto. All rights reserved.
 //
 
-import UIKit
+import FBSDKLoginKit
 
-class FacebookFriendsViewController: NSObject {
-
+class FacebookViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+        // Facebook access
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            returnUserData()
+        }
+        else {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "user_friends"]
+            loginView.delegate = self
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func returnUserData() {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                //                let friends : NSArray = result.valueForKey("user_friends") as! NSArray
+                //                print("User Friends are:")
+                //                print(friends)
+            }
+        })
+    }
+    
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if ((error) != nil) {
+            exit(-1)
+        }
+        else if result.isCancelled {
+            // Do nothing if cancelled
+        }
+        else {
+            returnUserData()
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {}
 }
