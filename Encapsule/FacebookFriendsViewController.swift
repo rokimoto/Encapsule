5cd67f9ad1b6a1e32eae5d7ceca233bd0a5ff739
Rachel Okimoto <rachel.okimoto@gmail.com>
2016-05-25 19:56:14 -0700

    adds a bunch of files that i am unsure of what it is doing

---
+++ Encapsule/FacebookFriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FacebookFriendsViewController.swift
+//  Encapsule
+//
+//  Created by Rachel Okimoto on 5/25/16.
+//  Copyright © 2016 Rachel Okimoto. All rights reserved.
+//
+
+import UIKit
+
+class FacebookFriendsViewController: NSObject {
+
+}
