c3e05e48977f840cc240dbf62c4ccdb4a7997c81
Rachel Okimoto <rachel.okimoto@gmail.com>
2016-06-06 22:48:06 -0700

    started hooking up to real ui

--- Encapsule/ViewController.swift
+++ Encapsule/ViewController.swift
@@ -7,20 +7,67 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+
 extension CALayer { func setBorderColorFromUIColor(color: UIColor) { self.borderColor = color.CGColor } }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    @IBOutlet weak var fbLoginBtn: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let background = CAGradientLayer().gradientColor()
         background.frame = self.view.bounds
         self.view.layer.insertSublayer(background, atIndex: 0)
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            returnUserData()
+        }
+        else {
+            self.fbLoginBtn.delegate = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func returnUserData() {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                //                let friends : NSArray = result.valueForKey("user_friends") as! NSArray
+                //                print("User Friends are:")
+                //                print(friends)
+            }
+        })
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if ((error) != nil) {
+            exit(-1)
+        }
+        else if result.isCancelled {
+            // Do nothing if cancelled
+        }
+        else {
+            returnUserData()
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {}
 }
 
